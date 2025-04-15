833f1a8bff99c3b526031fd3ba2f0acb5f08db34
Berna Kurt <beekurt98@gmail.com>
2025-04-15 22:00:59 +0300

    fix text not being in preview at start, add theme toggle

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,11 @@ function App() {
   const [input, setInput] = useState("")
   const [title, setTitle] = useState("welcome.md")
   const [leftMenu, setLeftMenu] = useState(false)
-  const [isEditing, setEditing] = useState(false)
-  const [allFiles, setAllFiles] = useState([{ title: "welcome", content: "", date: `${today.getDate()} ${(today.getMonth() + 1)} ${today.getFullYear()}` }])
+  const [allFiles, setAllFiles] = useState([{ title: "welcome.md", content: "", date: `${today.getDate()} ${(today.getMonth() + 1)} ${today.getFullYear()}` }])
   const [visible, setVisible] = useState(true)
 
   const inputRef = useRef(null)
   const deleteDialogRef = useRef(null)
-
-
 
   function handleChange(e) {
     let userInput = e.target.value
@@ -25,14 +22,26 @@ function App() {
     setInput(marked.parse(userInput))
   }
 
+  window.onload = () => {
+    setRawInput(rawInput)
+    setInput(marked.parse(rawInput))
+  }
+
   function handleLeftMenu() {
     setLeftMenu(!leftMenu)
   }
 
   function openNewDoc() {
-    setTitle("welcome")
+    setTitle("untitled.md")
     setInput("")
     setRawInput("")
+    const date = new Date()
+    const newFile = {
+      title: "untitled.md",
+      content: rawInput,
+      date: `${(date.getDate())} ${(date.getMonth() + 1)} ${date.getFullYear()}`
+    }
+    setAllFiles([...allFiles, newFile])
   }
 
   function handleSaveDoc() {
@@ -46,7 +55,6 @@ function App() {
   }
 
   function loadFile(index) {
-    setEditing(true)
     let selectedFile = allFiles[index]
     setTitle(selectedFile.title)
     setRawInput(selectedFile.content)
@@ -98,7 +106,7 @@ function App() {
                 <div className="markdown-title gray-title">
                   <p>MARKDOWN</p>
                 </div>
-                <UserInputArea rawInput={rawInput} inputRef={inputRef} handleChange={handleChange} input={input} isEditing={isEditing} />
+                <UserInputArea rawInput={rawInput} inputRef={inputRef} handleChange={handleChange} input={input} />
               </div>
               : ""
           }
@@ -147,12 +155,12 @@ function LeftMenu({ themeChange, openNewDoc, allFiles, loadFile }) {
               : ""
           }
         </div>
-        <button className="theme-switch" onClick={themeChange}>
-          <label class="switch">
-            <input type="checkbox" checked />
-            <span class="slider round"></span>
-          </label>
-        </button>
+        <label className="switch theme-switch" onChange={themeChange}>
+          <span className="sun"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="#ffd43b"><circle r="5" cy="12" cx="12"></circle><path d="m21 13h-1a1 1 0 0 1 0-2h1a1 1 0 0 1 0 2zm-17 0h-1a1 1 0 0 1 0-2h1a1 1 0 0 1 0 2zm13.66-5.66a1 1 0 0 1 -.66-.29 1 1 0 0 1 0-1.41l.71-.71a1 1 0 1 1 1.41 1.41l-.71.71a1 1 0 0 1 -.75.29zm-12.02 12.02a1 1 0 0 1 -.71-.29 1 1 0 0 1 0-1.41l.71-.66a1 1 0 0 1 1.41 1.41l-.71.71a1 1 0 0 1 -.7.24zm6.36-14.36a1 1 0 0 1 -1-1v-1a1 1 0 0 1 2 0v1a1 1 0 0 1 -1 1zm0 17a1 1 0 0 1 -1-1v-1a1 1 0 0 1 2 0v1a1 1 0 0 1 -1 1zm-5.66-14.66a1 1 0 0 1 -.7-.29l-.71-.71a1 1 0 0 1 1.41-1.41l.71.71a1 1 0 0 1 0 1.41 1 1 0 0 1 -.71.29zm12.02 12.02a1 1 0 0 1 -.7-.29l-.66-.71a1 1 0 0 1 1.36-1.36l.71.71a1 1 0 0 1 0 1.41 1 1 0 0 1 -.71.24z"></path></g></svg></span>
+          <span className="moon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="m223.5 32c-123.5 0-223.5 100.3-223.5 224s100 224 223.5 224c60.6 0 115.5-24.2 155.8-63.4 5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6-96.9 0-175.5-78.8-175.5-176 0-65.8 36-123.1 89.3-153.3 6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"></path></svg></span>
+          <input type="checkbox" className="input" />
+          <span className="slider"></span>
+        </label>
       </div>
     </>
   )
@@ -224,7 +232,7 @@ function DeleteDialog({ deleteDialogRef, handleDeleteDoc }) {
   )
 }
 
-function UserInputArea({ handleChange, input, inputRef, isEditing, rawInput }) {
+function UserInputArea({ handleChange, input, inputRef, rawInput }) {
 
   return (
     <>
